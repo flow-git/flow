7cf3b5517e4bba2d4f5feb481e1c37341d4ee4f6
Angela Dai <angeladai1@outlook.com>
2019-05-04 15:54:08 -0700

    added page functionality

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="sign-in">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     page: 'sign-in page'
+   }
+  }
+
+  signin = () => {
+    this.setState({page: 'data page'})
+  }
+
+  logout = () => {
+    this.setState({page: 'sign-in page'})
+  }
+
+  render() {
+    if (this.state.page === 'sign-in page') {
+      return (
+        <div className="sign-in">
+          <p> sign-in </p>
+          <button onClick={this.signin} class="button-home"> sign in </button>
+        </div>
+      );
+    } else if (this.state.page === 'data page') {
+      return (
+        <div className="App">
+          <p> data pages </p>
+          <button onClick={this.logout} class="button-home"> log out </button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
