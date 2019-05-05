46b112228ccf2d9fd5f6e9589db5e94134b48a7d
anlinpanda <anlin_c@yahoo.ca>
2019-05-05 12:12:58 -0700

    navbar addition

--- my-app/my-app/pages/Data.js
+++ my-app/my-app/pages/Data.js
@@ -7,7 +7,13 @@ const Data = () => (
         <title>Data Page</title>
         <link href="/static/App.css" rel="stylesheet"/>
     </Head>
+    <body>
       <p> data pages </p>
+      <nav className="navbar">
+      <p><img className="img-logo" src="../static/F.png"></img></p>
+      <p><img className="toolbar" src="../static/toolbar.png"></img></p>
+     </nav>
+      </body>
       <Link href="/">
       <button className="button-home"> log out </button>
       </Link>
--- my-app/my-app/pages/index.js
+++ my-app/my-app/pages/index.js
@@ -8,39 +8,41 @@ const signin = () => {
 }
 
 export default () => (
+  <body className="background">
     <div className="sign-in">
-        <Head>
-            <title>Login Page</title>
-            <link href="/static/App.css" rel="stylesheet" />
-        </Head>
+      <Head>
+        <title>Login Page</title>
+        <link href="/static/App.css" rel="stylesheet" />
+      </Head>
       <div className="sign-in-contents">
         <img src={logo} className="logo" alt="logo" />
-        <br/>
-        <br/>
+        <br />
+        <br />
         <div className="login-fields">
           <form>
             <div className="form-group">
-              <input type="email" className="form-control login-field" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="your email ..."/><br/>
-              <br/>
-              <input type="password" className="form-control login-field" id="exampleInputPassword1" placeholder="your password ..."/><br/>
+              <input type="email" className="form-control login-field" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="your email ..." /><br />
+              <br />
+              <input type="password" className="form-control login-field" id="exampleInputPassword1" placeholder="your password ..." /><br />
             </div>
-            <br/>
+            <br />
             <Link href="/Data">
-            <button type="submit" className="btn btn-primary login-button">log in</button>
+              <button type="submit" className="btn btn-primary login-button">log in</button>
             </Link>
           </form>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <div className="login-setup">
-            <p><i><b>first time?</b><br/>
-            setup your first Flow meter <u>here</u></i></p>
+            <p><i><b>first time?</b><br />
+              setup your first Flow meter <u>here</u></i></p>
           </div>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <div className="login-about">
             <p><u>about Flow</u> || <u>contact us</u> || <u>privacy policy</u></p>
           </div>
         </div>
       </div>
     </div>
-  );
+  </body>
+);
