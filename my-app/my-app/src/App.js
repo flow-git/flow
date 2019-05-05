a22fea8742386d7a73a24d1a0a3d4977fad31fe2
Angela Dai <angeladai1@outlook.com>
2019-05-04 19:08:01 -0700

    rendering sign-in page

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -24,10 +24,29 @@ class App extends Component {
         <div className="sign-in">
           <div className="sign-in-contents">
             <img src={logo} className="logo" alt="logo" />
+            <br/>
+            <br/>
             <div className="login-fields">
-              <input type="text" name="FirstName" value="your email ..."/><br/>
-              <input type="text" name="LastName" value="password ..."/>
-              <button onClick={this.signin} class="button-home">LOGIN</button>
+              <form>
+                <div class="form-group">
+                  <input type="email" class="form-control login-field" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="your email ..."/><br/>
+                  <br/>
+                  <input type="password" class="form-control login-field" id="exampleInputPassword1" placeholder="your password ..."/><br/>
+                </div>
+                <br/>
+                <button type="submit" class="btn btn-primary login-button" onClick={this.signin} >log in</button>
+              </form>
+              <br/>
+              <br/>
+              <div class="login-setup">
+                <p><i><b>first time?</b><br/>
+                setup your first Flow meter <u>here</u></i></p>
+              </div>
+              <br/>
+              <br/>
+              <div class="login-about">
+                <p><u>about Flow</u> || <u>contact us</u> || <u>privacy policy</u></p>
+              </div>
             </div>
           </div>
         </div>
